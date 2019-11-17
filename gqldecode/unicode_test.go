14e02ca490026fe464036f174e4918896e78dd2f
michaelraines <mike.g.raines@gmail.com>
2019-11-17 12:03:06 -0600

    remove test dependency on private repository

--- gqldecode/unicode_test.go
+++ gqldecode/unicode_test.go
@@ -2,11 +2,13 @@ package gqldecode
 
 import (
 	"testing"
-
-	"github.com/sprucehealth/backend/libs/test"
 )
 
 func TestIsValidPlane0Unicode(t *testing.T) {
-	test.Equals(t, true, IsValidPlane0Unicode(`This is a välid string`))
-	test.Equals(t, false, IsValidPlane0Unicode(`This is not 😡`))
+	if !IsValidPlane0Unicode(`This is a välid string`) {
+		t.Fail()
+	}
+	if IsValidPlane0Unicode(`This is not 😡`) {
+		t.Fail()
+	}
 }
